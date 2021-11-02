28833e6056eaf8d9f3b1fbe88b8eca0c676fb3a1
Jadon Lee <jadonjkl@gmail.com>
2021-11-02 15:12:56 -0800

    Added paths

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,11 @@
 
 package frc.robot;
 
-import java.io.FileSystem;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 
 /**
@@ -34,9 +33,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    try {
-      //Path trajectoryPath = FileSystem
-    }
+    
     m_robotContainer = new RobotContainer();
   }
 
